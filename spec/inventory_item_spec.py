bc586b9e671a9f8ae7d9198a60b914400d3df6ad
Javier Acero <j4cegu@gmail.com>
2013-05-16 20:59:13 +0200

    Refactor: extract item creation to setup

--- spec/inventory_item_spec.py
+++ spec/inventory_item_spec.py
@@ -11,10 +11,12 @@ class InventoryItem(object):
 
 with describe(InventoryItem) as _:
 
-     def it_has_an_id():
-        item = InventoryItem(IRRELEVANT_ID, IRRELEVANT_NAME)
-        expect(item.id).to.be.equal(IRRELEVANT_ID)
+    @before.each
+    def create_an_item():
+        _.item = InventoryItem(IRRELEVANT_ID, IRRELEVANT_NAME)
 
-     def it_has_a_name():
-        item = InventoryItem(IRRELEVANT_ID, IRRELEVANT_NAME)
-        expect(item.name).to.be.equal(IRRELEVANT_NAME)
+    def it_has_an_id():
+        expect(_.item.id).to.be.equal(IRRELEVANT_ID)
+
+    def it_has_a_name():
+        expect(_.item.name).to.be.equal(IRRELEVANT_NAME)
