2364f80cefbf939c72e546d9f783439581b5e753
Javier Acero <j4cegu@gmail.com>
2013-05-16 20:56:18 +0200

    Fail: inventory item should have an id

--- spec/inventory_item_spec.py
+++ spec/inventory_item_spec.py
@@ -1,14 +1,19 @@
 from mamba import describe, before
 from sure import expect
 
+IRRELEVANT_ID = 'id'
 IRRELEVANT_NAME = 'irrelevant name'
 
 class InventoryItem(object):
-    def __init__(self, name):
+    def __init__(self, id, name):
         self.name = name
 
 with describe(InventoryItem) as _:
 
+     def it_has_an_id():
+        item = InventoryItem(IRRELEVANT_ID, IRRELEVANT_NAME)
+        expect(item.id).to.be.equal(IRRELEVANT_ID)
+
      def it_has_a_name():
-        item = InventoryItem(IRRELEVANT_NAME)
+        item = InventoryItem(IRRELEVANT_ID, IRRELEVANT_NAME)
         expect(item.name).to.be.equal(IRRELEVANT_NAME)
