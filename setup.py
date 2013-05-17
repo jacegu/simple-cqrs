24548ff5f19fa191b354f50df25d74933e6a0d90
Javier Acero <j4cegu@gmail.com>
2013-05-17 21:47:05 +0200

    Infrastructure: properly setup the python project

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'Simple CQRS',
+    'description': "Port of Greg Young's m-r to python",
+    'author': 'Javier Acero',
+    'url': 'http://github.com/jacegu/simple-cqrs',
+    'download_url': 'http://github.com/jacegu/simple-cqrs',
+    'author_email': 'j4cegu@gmail.com',
+    'version': '0.1',
+    'install_requires': ['mamba'],
+    'packages': ['simplecqrs'],
+    'scripts': []
+}
+
+setup(**config)
