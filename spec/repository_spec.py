b3c792ebcd1e54b5ca250fd20584d0e262adf0f2
Javier Acero <j4cegu@gmail.com>
2013-05-21 19:02:00 +0200

    Refactor: swap to pyDoubles syntax

--- spec/repository_spec.py
+++ spec/repository_spec.py
@@ -1,9 +1,10 @@
 from mamba import describe, context, before
-from doublex import *
+from doublex.pyDoubles import *
 
 IRRELEVANT_ID = 'irrelevant id'
 IRRELEVANT_CHANGE1 = 'irrelevant change 1'
 IRRELEVANT_CHANGE2 = 'irrelevant change 2'
+CHANGES = [IRRELEVANT_CHANGE1, IRRELEVANT_CHANGE2]
 
 class Repository(object):
     def __init__(self, storage):
@@ -19,22 +20,22 @@ with describe(Repository) as _:
 
     @before.each
     def create_repository():
-        _.storage = Spy()
+        _.storage = spy()
         _.repository = Repository(_.storage)
 
     @before.each
     def create_aggregate():
-        _.aggregate = Spy()
+        _.aggregate = spy()
         _.aggregate.id = IRRELEVANT_ID
-        _.aggregate.uncommitted_changes = [IRRELEVANT_CHANGE1, IRRELEVANT_CHANGE2]
+        _.aggregate.uncommitted_changes = CHANGES
 
     with context('saving an aggregate'):
         def it_saves_all_uncommited_changes():
             _.repository.save(_.aggregate)
-            assert_that(_.storage.push, called().with_args(IRRELEVANT_ID, IRRELEVANT_CHANGE1))
-            assert_that(_.storage.push, called().with_args(IRRELEVANT_ID, IRRELEVANT_CHANGE2))
+            assert_that_method(_.storage.push).was_called().with_args(IRRELEVANT_ID, IRRELEVANT_CHANGE1)
+            assert_that_method(_.storage.push).was_called().with_args(IRRELEVANT_ID, IRRELEVANT_CHANGE2)
 
         def it_marks_the_changes_as_committed():
             _.repository.save(_.aggregate)
-            assert_that(_.aggregate.changes_committed, called())
+            assert_that_method(_.aggregate.changes_committed).was_called()
 
