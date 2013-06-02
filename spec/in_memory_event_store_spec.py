ad8a4033a6e2216a30add6b660cc2e04fadc86e5
Javier Acero <j4cegu@gmail.com>
2013-06-02 20:57:43 +0200

    Fail: getting aggregate events when there are some

---
+++ spec/in_memory_event_store_spec.py
@@ -0,0 +1,27 @@
+from mamba import describe, context, skip
+from sure import expect
+
+from spec.constants import *
+
+
+class InMemoryEventStore(object):
+    def save(self, aggregate_id, events, version):
+        raise NotImplementedError()
+
+
+with describe(InMemoryEventStore) as _:
+
+    with describe('getting events for an aggregate'):
+        with context('when the aggregate is found'):
+            def it_returns_the_aggregates_events():
+                _.event_store = InMemoryEventStore()
+                aggregate_id = IRRELEVANT_ID
+                _.event_store.save(aggregate_id, IRRELEVANT_EVENT, IRRELEVANT_VERSION)
+                expect(_.event_store.get_events_for_aggregate(aggregate_id)).to.be.equal(IRRELEVANT_EVENT)
+
+        with context('when no aggregate with provided id is found'):
+            @skip
+            def it_raises_an_aggregate_not_found_exception():
+                pass
+
+
