c2ef4e740f3b3b33459d45e71735963b59f35757
Javier Acero <j4cegu@gmail.com>
2013-06-03 08:55:24 +0200

    Refactor: extract InMemoryEventStore to the persistence module

--- spec/in_memory_event_store_spec.py
+++ spec/in_memory_event_store_spec.py
@@ -4,43 +4,7 @@ from doublex import *
 
 from spec.constants import *
 
-
-class InMemoryEventStore(object):
-    def __init__(self, publisher):
-        self.publisher = publisher
-        self.events = {}
-
-    def push(self, aggregate_id, event, version):
-        if self._there_are_events_for(aggregate_id):
-            self._verify_version(aggregate_id, version)
-        self._store(aggregate_id, event, version)
-        self.publisher.publish(event)
-
-    def get_events_for_aggregate(self, aggregate_id):
-        if self._there_are_events_for(aggregate_id):
-            return [event_data['event'] for event_data in self.events[aggregate_id]]
-        else:
-            raise AggregateNotFoundError()
-
-    def _there_are_events_for(self, aggregate_id):
-        return aggregate_id in self.events
-
-    def _verify_version(self, aggregate_id, provided_version):
-        if provided_version != self._next_version_of(aggregate_id):
-            raise ConcurrencyError()
-
-    def _next_version_of(self, aggregate_id):
-        return self.events.get(aggregate_id)[-1].get('version') + 1
-
-    def _store(self, aggregate_id, event, version):
-        self.events.setdefault(aggregate_id, []).append({'event': event, 'version': version})
-
-
-class AggregateNotFoundError(RuntimeError):
-    pass
-
-class ConcurrencyError(RuntimeError):
-    pass
+from simplecqrs.persistence import InMemoryEventStore, AggregateNotFoundError, ConcurrencyError
 
 
 with describe(InMemoryEventStore) as _:
