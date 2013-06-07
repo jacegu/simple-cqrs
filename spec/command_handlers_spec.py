b99b8bff31a21c3e9e0ff4b06ad70dce55286553
Javier Acero <j4cegu@gmail.com>
2013-06-07 15:06:32 +0200

    Pass

--- spec/command_handlers_spec.py
+++ spec/command_handlers_spec.py
@@ -9,6 +9,15 @@ from simplecqrs.persistence import Repository
 from simplecqrs.inventory import InventoryItem
 from simplecqrs.commands import *
 
+
+class InventoryCommandsHandler(object):
+    def __init__(self, inventory_item_repository):
+        self.inventory_item_repository = inventory_item_repository
+
+    def handle(self, command):
+        self.inventory_item_repository.save(InventoryItem(command.item_id, command.item_name))
+
+
 with describe('InventoryCommandsHandler') as _:
 
     def it_can_handle_a_create_inventory_item_command():
