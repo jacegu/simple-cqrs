af229cb935fc59fe4719b0411a0fe6b448a49573
Javier Acero <j4cegu@gmail.com>
2013-06-07 17:54:48 +0200

    Refactor: remove duplication in specs

--- spec/command_handlers_spec.py
+++ spec/command_handlers_spec.py
@@ -42,25 +42,22 @@ with describe('InventoryCommandsHandler') as _:
 
     def it_can_handle_a_create_inventory_item_command():
         _.handler.handle(CreateInventoryItem(IRRELEVANT_ID, IRRELEVANT_NAME))
-
         assert_that(_.repository.save, called().with_args(instance_of(InventoryItem)))
 
     def it_can_handle_rename_inventory_item_commands():
-        item = Spy(InventoryItem)
-        with _.repository as r: r.find_by_id(IRRELEVANT_ID).returns(item)
-
+        item = _stubbed_item()
         _.handler.handle(RenameInventoryItem(IRRELEVANT_ID, IRRELEVANT_NAME, IRRELEVANT_VERSION))
-
         assert_that(item.rename, called().with_args(IRRELEVANT_NAME))
         assert_that(_.repository.save, called().with_args(item, IRRELEVANT_VERSION))
 
     def it_can_handle_deactivate_inventory_item_commands():
-        item = Spy(InventoryItem)
-        with _.repository as r: r.find_by_id(IRRELEVANT_ID).returns(item)
-
+        item = _stubbed_item()
         _.handler.handle(DeactivateInventoryItem(IRRELEVANT_ID, IRRELEVANT_VERSION))
-
         assert_that(item.deactivate, called())
         assert_that(_.repository.save, called().with_args(item, IRRELEVANT_VERSION))
 
+    def _stubbed_item():
+        item = Spy(InventoryItem)
+        with _.repository as r: r.find_by_id(IRRELEVANT_ID).returns(item)
+        return item
 
