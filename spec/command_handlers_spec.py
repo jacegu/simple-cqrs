04c585a76415ca426d812a7ec2a6ee6d2d7f9750
Javier Acero <j4cegu@gmail.com>
2013-06-07 17:50:39 +0200

    Refactor: use context manager to get and save items

--- spec/command_handlers_spec.py
+++ spec/command_handlers_spec.py
@@ -8,7 +8,7 @@ from spec.constants import *
 from simplecqrs.persistence import Repository
 from simplecqrs.inventory import InventoryItem
 from simplecqrs.commands import *
-
+from contextlib import contextmanager
 
 class InventoryCommandsHandler(object):
     def __init__(self, inventory_item_repository):
@@ -19,16 +19,18 @@ class InventoryCommandsHandler(object):
             self.inventory_item_repository.save(InventoryItem(command.item_id, command.item_name))
 
         if command.__class__ == RenameInventoryItem:
-            item = self.inventory_item_repository.find_by_id(command.item_id)
-            item.rename(command.new_item_name)
-            self.inventory_item_repository.save(item, command.original_version)
+            with self._inventory_item(command.item_id, command.original_version) as item:
+                item.rename(command.new_item_name)
 
         if command.__class__ == DeactivateInventoryItem:
-            item = self.inventory_item_repository.find_by_id(command.item_id)
-            item.deactivate()
-            self.inventory_item_repository.save(item, command.original_version)
+            with self._inventory_item(command.item_id, command.original_version) as item:
+                item.deactivate()
 
-
+    @contextmanager
+    def _inventory_item(self, item_id, item_version):
+        item = self.inventory_item_repository.find_by_id(item_id)
+        yield item
+        self.inventory_item_repository.save(item, item_version)
 
 
 with describe('InventoryCommandsHandler') as _:
