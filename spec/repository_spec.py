606eb1ca037e19630c0d05f06447504c53d604c6
Javier Acero <j4cegu@gmail.com>
2013-06-08 17:58:47 +0200

    Fail: interface inconsistence between specs and implementation

--- spec/repository_spec.py
+++ spec/repository_spec.py
@@ -4,7 +4,7 @@ from sure import expect
 
 from spec.constants import *
 
-from simplecqrs.persistence import Repository
+from simplecqrs.persistence import Repository, InMemoryEventStore
 from simplecqrs.aggregate import Aggregate
 
 CHANGES = [IRRELEVANT_CHANGE1, IRRELEVANT_CHANGE2]
@@ -13,7 +13,7 @@ with describe(Repository) as _:
 
     @before.each
     def create_repository():
-        _.storage = Spy()
+        _.storage = Spy(InMemoryEventStore)
         _.aggregate_class = Spy()
         _.repository = Repository(_.aggregate_class, _.storage)
 
@@ -45,11 +45,11 @@ with describe(Repository) as _:
 
     with context('finding an aggregate by id'):
         def it_returns_the_aggregate():
-            with _.storage: _.storage.get_aggregate_changes(IRRELEVANT_ID).returns(CHANGES)
+            with _.storage: _.storage.get_events_for_aggregate(IRRELEVANT_ID).returns(CHANGES)
             with _.aggregate_class as klass: klass.from_events(CHANGES).returns(_.aggregate)
             expect(_.repository.find_by_id(IRRELEVANT_ID)).to.be.equal(_.aggregate)
 
         def it_returns_none_when_no_aggregate_with_provided_id_is_found():
-            with _.storage: _.storage.get_aggregate_changes(IRRELEVANT_ID).returns([])
+            with _.storage: _.storage.get_events_for_aggregate(IRRELEVANT_ID).returns([])
             expect(_.repository.find_by_id(IRRELEVANT_ID)).to.be.equal(None)
             assert_that(_.aggregate_class.from_events, never(called()))
