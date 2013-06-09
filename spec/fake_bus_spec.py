208592697a18247e049d5ce6bcdb62d0e59f723e
Javier Acero <j4cegu@gmail.com>
2013-06-09 12:28:13 +0200

    Refactor: extract FakeBus to its own file

--- spec/fake_bus_spec.py
+++ spec/fake_bus_spec.py
@@ -2,39 +2,15 @@ from mamba import describe, context, before, skip
 from sure import *
 from doublex import *
 
-
 from spec.constants import *
 
+from simplecqrs.fake_bus import FakeBus
 from simplecqrs.errors import InvalidOperationError
 
-class FakeBus(object):
-    def __init__(self):
-        self.routes = {}
-
-    def register_handler(self, event_or_command, handler):
-        self.routes.setdefault(event_or_command, []).append(handler)
-
-    def send(self, command):
-        self._handler_for(command).handle(command)
-
-    def _handler_for(self, command):
-        self._check_that_has_handlers(command)
-        self._check_that_has_a_single_handler(command)
-        return self._all_handers_for(command)[0]
-
-    def _check_that_has_handlers(self, command):
-        if not type(command) in self.routes:
-            raise(InvalidOperationError('No handler for command ' + str(type(command)) + ' found'))
-
-    def _check_that_has_a_single_handler(self, command):
-        if len(self._all_handers_for(command)) > 1:
-            raise(InvalidOperationError(str(type(command)) + ' has more than one handler'))
-
-    def _all_handers_for(self, command):
-        return self.routes[type(command)]
 
 class DummyCommand(object):
     pass
+
 
 with describe('FakeBus') as _:
 
