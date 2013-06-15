16ea2efa0e00eb7e8d7ddf887bb1c57296e23fd3
Javier Acero <j4cegu@gmail.com>
2013-06-15 19:33:17 +0200

    Pass

--- simplecqrs/fake_bus.py
+++ simplecqrs/fake_bus.py
@@ -11,20 +11,21 @@ class FakeBus(object):
         self._handler_for(command).handle(command)
 
     def publish(self, event):
-        self._handler_for(event).handle(event)
+        for handler in self._all_handlers_for(event):
+            handler.handle(event)
 
-    def _handler_for(self, event_or_command):
-        self._check_that_has_handlers(event_or_command)
-        self._check_that_has_a_single_handler(event_or_command)
-        return self._all_handers_for(event_or_command)[0]
+    def _handler_for(self, command):
+        self._check_that_has_handlers(command)
+        self._check_that_has_a_single_handler(command)
+        return self._all_handlers_for(command)[0]
 
-    def _check_that_has_handlers(self, event_or_command):
-        if not type(event_or_command) in self.routes:
-            raise(InvalidOperationError('No handler for event_or_command ' + str(type(event_or_command)) + ' found'))
+    def _check_that_has_handlers(self, command):
+        if not type(command) in self.routes:
+            raise(InvalidOperationError('No handler for command ' + str(type(command)) + ' found'))
 
-    def _check_that_has_a_single_handler(self, event_or_command):
-        if len(self._all_handers_for(event_or_command)) > 1:
-            raise(InvalidOperationError(str(type(event_or_command)) + ' has more than one handler'))
+    def _check_that_has_a_single_handler(self, command):
+        if len(self._all_handlers_for(command)) > 1:
+            raise(InvalidOperationError(str(type(command)) + ' has more than one handler'))
 
-    def _all_handers_for(self, event_or_command):
-        return self.routes[type(event_or_command)]
+    def _all_handlers_for(self, event_or_command):
+        return self.routes.get(type(event_or_command), [])
