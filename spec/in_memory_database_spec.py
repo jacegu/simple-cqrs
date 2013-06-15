a9cf3a088ed313c58e44c5da60e34ded3066a587
Javier Acero <j4cegu@gmail.com>
2013-06-15 22:15:06 +0200

    Fail: adding InventoryItemDtos to the database

--- spec/in_memory_database_spec.py
+++ spec/in_memory_database_spec.py
@@ -1,10 +1,19 @@
 from mamba import describe, context
 from sure import *
 
+from spec.constants import *
+
 class InMemoryDataBase(object):
     @property
     def inventory_items(self):
         return []
+
+    def add(self, inventory_item_dto):
+        pass
+
+class InventoryItemDto(object):
+    def __init__(self, id, name):
+        pass
 
 with describe('InMemoryDataBase') as _:
 
@@ -12,6 +21,11 @@ with describe('InMemoryDataBase') as _:
         def it_starts_with_no_inventory_item_dtos():
             db = InMemoryDataBase()
             expect(db.inventory_items).to.be.empty
+
+        def it_can_add_inventory_item_dtos():
+            db = InMemoryDataBase()
+            db.add(InventoryItemDto(IRRELEVANT_ID, IRRELEVANT_NAME))
+            expect(db.inventory_items).to.have.length_of(1)
 
     with context('inventory item details'):
         pass
