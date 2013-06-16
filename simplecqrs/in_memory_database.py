36b136469113079887b4cf8b9231cddff8ed0582
Javier Acero <j4cegu@gmail.com>
2013-06-16 08:19:56 +0200

    Refactor: extract InMemoryDatabase to its own file

---
+++ simplecqrs/in_memory_database.py
@@ -0,0 +1,27 @@
+class InMemoryDataBase(object):
+    def __init__(self):
+        self.inventory_items_dtos = {}
+        self.inventory_item_details_dtos = {}
+
+    @property
+    def inventory_items(self):
+        return self.inventory_items_dtos.values()
+
+    def add_inventory_item(self, inventory_item_dto):
+        self.inventory_items_dtos[inventory_item_dto.id] = inventory_item_dto
+
+    def remove_inventory_item(self, inventory_item_id):
+        del(self.inventory_items_dtos[inventory_item_id])
+
+    @property
+    def inventory_item_details(self):
+        return self.inventory_item_details_dtos.values()
+
+    def add_inventory_item_details(self, inventory_item_details_dto):
+        self.inventory_item_details_dtos[inventory_item_details_dto.id] = inventory_item_details_dto
+
+    def remove_inventory_item_details(self, inventory_item_details_id):
+        del(self.inventory_item_details_dtos[inventory_item_details_id])
+
+    def get_inventory_item_details(self, inventory_item_details_id):
+        return self.inventory_item_details_dtos[inventory_item_details_id]
--- spec/in_memory_database_spec.py
+++ spec/in_memory_database_spec.py
@@ -3,35 +3,7 @@ from sure import *
 
 from spec.constants import *
 
-
-class InMemoryDataBase(object):
-    def __init__(self):
-        self.inventory_items_dtos = {}
-        self.inventory_item_details_dtos = {}
-
-    @property
-    def inventory_items(self):
-        return self.inventory_items_dtos.values()
-
-    def add_inventory_item(self, inventory_item_dto):
-        self.inventory_items_dtos[inventory_item_dto.id] = inventory_item_dto
-
-    def remove_inventory_item(self, inventory_item_id):
-        del(self.inventory_items_dtos[inventory_item_id])
-
-    @property
-    def inventory_item_details(self):
-        return self.inventory_item_details_dtos.values()
-
-    def add_inventory_item_details(self, inventory_item_details_dto):
-        self.inventory_item_details_dtos[inventory_item_details_dto.id] = inventory_item_details_dto
-
-    def remove_inventory_item_details(self, inventory_item_details_id):
-        del(self.inventory_item_details_dtos[inventory_item_details_id])
-
-    def get_inventory_item_details(self, inventory_item_details_id):
-        return self.inventory_item_details_dtos[inventory_item_details_id]
-
+from simplecqrs.in_memory_database import InMemoryDataBase
 
 class InventoryItemDto(object):
     def __init__(self, id, name):
